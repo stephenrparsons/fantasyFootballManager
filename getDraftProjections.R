def88e0c17e2628087f8eed924b4e4a36b8031c8
Stephen Parsons <stephen.r.parsons@gmail.com>
2014-08-29 10:22:17 -0400

    format incoming data

--- getDraftProjections.R
+++ getDraftProjections.R
@@ -5,9 +5,19 @@ rb <- readHTMLTable("http://www.fantasypros.com/nfl/projections/rb.php", strings
 wr <- readHTMLTable("http://www.fantasypros.com/nfl/projections/wr.php", stringsAsFactors = FALSE)$data
 te <- readHTMLTable("http://www.fantasypros.com/nfl/projections/te.php", stringsAsFactors = FALSE)$data
 k  <- readHTMLTable("http://www.fantasypros.com/nfl/projections/k.php",  stringsAsFactors = FALSE)$data
+d  <- readHTMLTable("http://games.espn.go.com/ffl/tools/projections?leagueId=0&slotCategoryId=16", stringsAsFactors = FALSE)$playertable_0
 
+colnames(d) <- d[1,]
+d <- d[-1,][c("PLAYER, TEAM POS", "PTS")]
+colnames(d) <- c("Player", "FPTS")
+d[,1] <- sapply(d[,1], function(elem) unlist(strsplit(elem, ' '))[1])
+
+qb$POS <- "QB"
+rb$POS <- "RB"
+wr$POS <- "WR"
+te$POS <- "TE"
+k$POS  <- "K"
+d$POS  <- "D/ST"
+
+print((d))
 print(head(qb))
-print(head(rb))
-print(head(wr))
-print(head(te))
-print(head(k ))
