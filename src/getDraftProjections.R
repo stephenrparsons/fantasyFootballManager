743a5aeefd504b389406637f0a056855b948aa55
Stephen Parsons <stephen.r.parsons@gmail.com>
2017-08-17 17:39:09 -0400

    update with 2017 links and data

--- src/getDraftProjections.R
+++ src/getDraftProjections.R
@@ -2,12 +2,12 @@ library(XML)
 library(RCurl)
 library(plyr)
 
-qbURL <- "https://www.fantasypros.com/nfl/projections/qb.php"
+qbURL <- "https://www.fantasypros.com/nfl/projections/qb.php?week=draft"
 qb <- readHTMLTable(getURL(qbURL), stringsAsFactors = FALSE)$data
-rb <- readHTMLTable(getURL("https://www.fantasypros.com/nfl/projections/rb.php"), stringsAsFactors = FALSE)$data
-wr <- readHTMLTable(getURL("https://www.fantasypros.com/nfl/projections/wr.php"), stringsAsFactors = FALSE)$data
-te <- readHTMLTable(getURL("https://www.fantasypros.com/nfl/projections/te.php"), stringsAsFactors = FALSE)$data
-k  <- readHTMLTable(getURL("https://www.fantasypros.com/nfl/projections/k.php"),  stringsAsFactors = FALSE)$data
+rb <- readHTMLTable(getURL("https://www.fantasypros.com/nfl/projections/rb.php?week=draft"), stringsAsFactors = FALSE)$data
+wr <- readHTMLTable(getURL("https://www.fantasypros.com/nfl/projections/wr.php?week=draft"), stringsAsFactors = FALSE)$data
+te <- readHTMLTable(getURL("https://www.fantasypros.com/nfl/projections/te.php?week=draft"), stringsAsFactors = FALSE)$data
+k  <- readHTMLTable(getURL("https://www.fantasypros.com/nfl/projections/k.php?week=draft"),  stringsAsFactors = FALSE)$data
 d  <- readHTMLTable(getURL("http://games.espn.com/ffl/tools/projections?leagueId=0&slotCategoryId=16"), stringsAsFactors = FALSE)$playertable_0
 
 colnames(d) <- d[1,]
